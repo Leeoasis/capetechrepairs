c1d41bc2af28e166a5ab01d59f22fd20d586f0b1
Leslie Gudo <Kooliesk@gmail.com>
2023-11-13 16:45:44 +0200

    create navbar routes

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Home
+      </h1>
+    </div>
   )
 }
+
+export default Home
