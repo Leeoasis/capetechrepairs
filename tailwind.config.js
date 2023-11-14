4197e20bfb08ae3486712c3f6a6d5f3648d793cc
Leslie Gudo <Kooliesk@gmail.com>
2023-11-14 11:44:09 +0200

    configure custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,22 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'custom-image': 'url("/board.jpeg")',
       },
+      backgroundColor: {
+        'overlay': 'rgba(0, 0, 255, 0.6)',
+      },
+      zIndex: {
+        '10': 10,
+        '20': 20,
+      },
+      container: {
+        center: true,
+      },
+      colors: {
+        customGreen: 'rgba(255, 165, 0, 1)',
+      },
+    
     },
   },
   plugins: [],
